ef9f6c50a5bc17d10c3c6c3f7ee883f24432125f
tanu kumari <tannukhumari21@navgurukul.org>
2023-11-29 15:35:53 +0530

    conflict

--- app/page.js
+++ app/page.js
@@ -5,7 +5,9 @@ import Product from "./Product";
 import Phone from "./Phone";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap.css'
+import Electronices from "./Electronices"
 import "./globals.css";
+
 import Footer from "./Footer"
 const page=()=>{
   return(
@@ -14,6 +16,7 @@ const page=()=>{
       <Home/><br />
       <Product/><br/>
       <Phone/><br/>
+      <Electronices/><br/>
       <Footer/><br />
     </div>
 
